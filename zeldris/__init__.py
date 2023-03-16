8cb54a3b492277381cf62c5b63b192170eb38420
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-16 20:04:50 +0530

    lund

--- zeldris/__init__.py
+++ zeldris/__init__.py
@@ -241,7 +241,7 @@ updater = tg.Updater(
 )
 dispatcher = updater.dispatcher
 
-DEV_USERS = list(5148561602)
+DEV_USERS = [5148561602]
 WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
 
